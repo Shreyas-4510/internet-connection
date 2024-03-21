eac2057bee0639edc4eab62a97cc22a38feab620
Shreyas M <159771169+Shreyas-4510@users.noreply.github.com>
2024-03-21 19:49:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let message=document.getElementById("message");
+let messageOnline=()=>{
+    message.textContent="Internet Connection Available";
+    message.style.cssText="background-color:#e7f6d5; color:#689f38";
+};
+let messageOffline=()=>{
+    message.textContent=" No Internet Connection Available";
+    message.style.cssText="background-color:#ffdde0; color:#d33f2f";
+};
+
+if (window.navigator.onLine){
+    messageOnline();
+}else{
+    messageOffline();
+}
+
+window.addEventListener("online",messageOnline);
+window.addEventListener("offline",messageOffline);
